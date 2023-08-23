46a2906e7c31e6f71402493925e9842e6a8150a1
Parshant <parshant4321@gmail.com>
2023-08-23 17:46:33 +0500

    till Video 4

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ function App() {
 
   
   return (
-    <div className="App">
+    <>
+    <div className='blank'>
+      Hello
+    </div>
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -20,7 +24,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </div></>
   );
 }
 
